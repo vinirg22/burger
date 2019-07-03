3f3e1b005bd8731a7101b88cae7cdb4b2071225a
vinirg22 <aramburo.irving@gmail.com>
2019-07-03 16:44:28 -0700

    app layout set up, files created for burgers app

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Chesseburger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Sandwich');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno cheddar', true);
